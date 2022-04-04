5c3c0569ea076766845517ef601ca41af50e3d6a
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-04-04 19:33:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-// начальные данные
 List<Shoping> products = new List<Shoping>
 {
     new() { Id = Guid.NewGuid().ToString(), Name = "Milk", Quantity = 1, Comment = "1%" },
@@ -16,44 +15,33 @@ app.MapGet("/api/products", () => products);
 
 app.MapGet("/api/products/{id}", (string id) =>
 {
-    // получаем продукт по id
     Shoping? product = products.FirstOrDefault(p => p.Id == id);
-    // если не найден, отправляем статусный код и сообщение об ошибке
     if (product == null) return Results.NotFound(new { message = "Product not found!" });
 
-    // если пользователь найден, отправляем его
     return Results.Json(product);
 });
 
 app.MapDelete("/api/products/{id}", (string id) =>
 {
-    // получаем продукт по id
     Shoping? product = products.FirstOrDefault(p => p.Id == id);
 
-    // если не найден, отправляем статусный код и сообщение об ошибке
     if (product == null) return Results.NotFound(new { message = "Product not found!" });
 
-    // если пользователь найден, удаляем его
     products.Remove(product);
     return Results.Json(product);
 });
 
 app.MapPost("/api/products", (Shoping product) => {
 
-    // устанавливаем id для нового продукта
     product.Id = Guid.NewGuid().ToString();
-    // добавляем продукт в список
     products.Add(product);
     return product;
 });
 
 app.MapPut("/api/products", (Shoping productData) => {
 
-    // получаем продукт по id
     var product = products.FirstOrDefault(p => p.Id == productData.Id);
-    // если не найден, отправляем статусный код и сообщение об ошибке
     if (product == null) return Results.NotFound(new { message = "Product not found!" });
-    // если продукта найден, изменяем его данные и отправляем обратно клиенту
 
     product.Name = productData.Name;
     product.Quantity = productData.Quantity;
